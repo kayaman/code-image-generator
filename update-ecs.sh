2f9a687dc24fbf2e350f1a372a198ea4a7759683
Marco Antonio Gonzalez Junior <m@rco.sh>
2023-08-25 08:43:02 -0300

    Dockerize

--- update-ecs.sh
+++ update-ecs.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker build -t code-image-generator:latest .
 aws ecr get-login-password --region sa-east-1 | docker login --username AWS --password-stdin 935614717044.dkr.ecr.sa-east-1.amazonaws.com
 docker tag code-image-generator:latest 935614717044.dkr.ecr.sa-east-1.amazonaws.com/code-image-generator:latest
